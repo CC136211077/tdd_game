2570826cb878b32b143ad981b9e92ed41f1f6e8b
CC136211077 <136211077@qq.com>
2020-12-31 17:42:56 +0800

    [20201231]delete:删除搜索第一个宝箱的测试方法

--- src/test/java/com/cc/SearchTreasureBoxTest.java
+++ src/test/java/com/cc/SearchTreasureBoxTest.java
@@ -1,46 +1,16 @@
 package com.cc;
 
-import com.cc.utils.HashUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class SearchTreasureBoxTest {
 
     @Before
     public void setUp() throws Exception {
     }
-
-    @Test
-    /**
-     * 已知晓第一个钥匙查询所有
-     * 如果: given 数字遍历写在查询里面
-     * 当:  when 经过遍历查询后
-     * 则:  then 返回一个神秘钥匙数组
-     */
-    public void searchFirst() {
-        SearchTreasureBox searchTreasureBox = new SearchTreasureBox();
-//        for(int i=1;i<1000000000;i++){
-//
-//            String s = searchTreasureBox.searchFirst(i);
-//            System.out.println(s);
-//            if(s.equals("00000")){
-//                System.out.println(i);
-//                break;
-//            }
-//        }
-        List<Integer> integers = searchTreasureBox.searchFirst(3229170);
-        integers.stream().forEach(e-> System.out.println(e));
-        if (integers.size()>0) {
-            integers.stream().forEach(e-> System.out.println(e));
-        }
-        Assert.assertEquals(10,integers.size());
-    }
-
     @Test
     /**
      * 获取所有宝箱的钥匙
